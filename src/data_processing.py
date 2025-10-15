8143cc0010a9a999fcbcdcc22b489dda2f9a25ee
Faizack <faizack619@gmail.com>
2025-10-16 02:27:10 +0530

    added parameter for dvc

--- src/data_processing.py
+++ src/data_processing.py
@@ -6,30 +6,54 @@ from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 import string
 import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
+
+nltk.download("stopwords")
+nltk.download("punkt")
+import yaml
 
 # Ensure the "logs" directory exists
-log_dir = 'logs'
+log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
 
 # Setting up logger
-logger = logging.getLogger('data_preprocessing')
-logger.setLevel('DEBUG')
+logger = logging.getLogger("data_preprocessing")
+logger.setLevel("DEBUG")
 
 console_handler = logging.StreamHandler()
-console_handler.setLevel('DEBUG')
+console_handler.setLevel("DEBUG")
 
-log_file_path = os.path.join(log_dir, 'data_preprocessing.log')
+log_file_path = os.path.join(log_dir, "data_preprocessing.log")
 file_handler = logging.FileHandler(log_file_path)
-file_handler.setLevel('DEBUG')
+file_handler.setLevel("DEBUG")
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 console_handler.setFormatter(formatter)
 file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+
+def load_params(params_path: str):
+    """
+    Load Param config Files
+    """
+    try:
+        with open(params_path, "r") as file:
+            params = yaml.safe_load(file)
+
+        logger.debug("Parameters retrieved from %s", params_path)
+        return params
+    except FileNotFoundError:
+        logger.error("File not found: %s", params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error("YAML error: %s", e)
+        raise
+    except Exception as e:
+        logger.error("Unexpected error: %s", e)
+        raise
+
 
 def transform_text(text):
     """
@@ -43,48 +67,54 @@ def transform_text(text):
     # Remove non-alphanumeric tokens
     text = [word for word in text if word.isalnum()]
     # Remove stopwords and punctuation
-    text = [word for word in text if word not in stopwords.words('english') and word not in string.punctuation]
+    text = [
+        word
+        for word in text
+        if word not in stopwords.words("english") and word not in string.punctuation
+    ]
     # Stem the words
     text = [ps.stem(word) for word in text]
     # Join the tokens back into a single string
     return " ".join(text)
 
-def preprocess_df(df, text_column='text', target_column='target'):
+
+def preprocess_df(df, text_column="text", target_column="target"):
     """
     Preprocesses the DataFrame by encoding the target column, removing duplicates, and transforming the text column.
     """
     try:
-        logger.debug('Starting preprocessing for DataFrame')
+        logger.debug("Starting preprocessing for DataFrame")
         # Encode the target column
         encoder = LabelEncoder()
         df[target_column] = encoder.fit_transform(df[target_column])
-        logger.debug('Target column encoded')
+        logger.debug("Target column encoded")
 
         # Remove duplicate rows
-        df = df.drop_duplicates(keep='first')
-        logger.debug('Duplicates removed')
-        
+        df = df.drop_duplicates(keep="first")
+        logger.debug("Duplicates removed")
+
         # Apply text transformation to the specified text column
         df.loc[:, text_column] = df[text_column].apply(transform_text)
-        logger.debug('Text column transformed')
+        logger.debug("Text column transformed")
         return df
-    
+
     except KeyError as e:
-        logger.error('Column not found: %s', e)
+        logger.error("Column not found: %s", e)
         raise
     except Exception as e:
-        logger.error('Error during text normalization: %s', e)
+        logger.error("Error during text normalization: %s", e)
         raise
 
-def main(text_column='text', target_column='target'):
+
+def main(text_column="text", target_column="target"):
     """
     Main function to load raw data, preprocess it, and save the processed data.
     """
     try:
         # Fetch the data from data/raw
-        train_data = pd.read_csv('./data/raw/train.csv')
-        test_data = pd.read_csv('./data/raw/test.csv')
-        logger.debug('Data loaded properly')
+        train_data = pd.read_csv("./data/raw/train.csv")
+        test_data = pd.read_csv("./data/raw/test.csv")
+        logger.debug("Data loaded properly")
 
         # Transform the data
         train_processed_data = preprocess_df(train_data, text_column, target_column)
@@ -93,18 +123,23 @@ def main(text_column='text', target_column='target'):
         # Store the data inside data/processed
         data_path = os.path.join("./data", "interim")
         os.makedirs(data_path, exist_ok=True)
-        
-        train_processed_data.to_csv(os.path.join(data_path, "train_processed.csv"), index=False)
-        test_processed_data.to_csv(os.path.join(data_path, "test_processed.csv"), index=False)
-        
-        logger.debug('Processed data saved to %s', data_path)
+
+        train_processed_data.to_csv(
+            os.path.join(data_path, "train_processed.csv"), index=False
+        )
+        test_processed_data.to_csv(
+            os.path.join(data_path, "test_processed.csv"), index=False
+        )
+
+        logger.debug("Processed data saved to %s", data_path)
     except FileNotFoundError as e:
-        logger.error('File not found: %s', e)
+        logger.error("File not found: %s", e)
     except pd.errors.EmptyDataError as e:
-        logger.error('No data: %s', e)
+        logger.error("No data: %s", e)
     except Exception as e:
-        logger.error('Failed to complete the data transformation process: %s', e)
+        logger.error("Failed to complete the data transformation process: %s", e)
         print(f"Error: {e}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
